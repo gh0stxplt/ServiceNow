a80adc352b938c16646950713480ca6efdb44e42
Gh0stxplt <gh0stxplt@pm.me>
2022-11-17 21:32:48 -0500

    Stale Interactions Cleanup Job

---
+++ scheduled_jobs/staleInteractions Scheduled Job.js
@@ -0,0 +1,13 @@
+/*
+*   Since there is no OOB behavior for closing stale interactions, this scheduled job
+*   will query all active Phone type interactions from the prior day and update them 
+*   to closed complete. Date range can be customized with a new encoded query if needed.
+*/
+var staleInteractions = new GlideRecord('interaction');
+staleInteractions.addEncodedQuery("active=true^type=Phone^opened_at<javascript:gs.beginningOfToday()");
+staleInteractions.query();
+while (staleInteractions.next()) {
+    staleInteractions.setValue('state', 'closed_complete');
+    staleInteractions.update();
+};
+gs.log("Stale interactions closed on " + new GlideDateTime().getDate() + " - " + staleInteractions.getRowCount());
